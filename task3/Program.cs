35d62cc35d2d1cd4951315e5defc9865a0c3cc09
maxwbk <maxwbk@yandex.ru>
2023-06-26 22:33:58 +0700

    task3 ready

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Таблица кубов чисел от 1 до {0}", N);
+Console.WriteLine("------------------------------");
+
+for (int i = 1; i <= N; i++)
+{
+    int cube = i * i * i;
+    Console.WriteLine("{0}^3 = {1}", i, cube);
+}
+
+Console.ReadLine();
+
